33e64bab600b7b1e2a2bb21540aa24c9de7d3863
ztsv-av <ztsv.1337@gmail.com>
2024-11-29 11:57:18 +0100

    add new global vars

--- utils/vars.py
+++ utils/vars.py
@@ -5,5 +5,11 @@ import torch
 SEED = 1
 # define the device globally
 DEVICE = torch.device("cpu")
+# mnist data path
+MNIST_PATH = "data"
 # trained mnist model path
 MODEL_PATH = "model/trained/mnist_model.pth"
+# number of epochs for training
+EPOCHS = 2
+# learning rate
+LR = 0.01
