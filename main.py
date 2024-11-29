877f968b678296566478a8af92d994e18fe7eb44
ztsv-av <ztsv.1337@gmail.com>
2024-11-29 11:59:05 +0100

    main script to call other scripts from cmd

---
+++ main.py
@@ -0,0 +1,9 @@
+from data.data import get_mnist_dataloader
+from model.train import train_model
+from model.evaluate import evaluate_model
+
+def main():
+    evaluate_model()
+
+if __name__ == "__main__":
+    main()
