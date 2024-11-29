46d6e395e7a64c4f4b23c971a0d846f293877a38
ztsv-av <ztsv.1337@gmail.com>
2024-11-29 11:58:15 +0100

    add  metric calculation to both train and evaluate functions; add load_model function to evaluate script

--- model/evaluate.py
+++ model/evaluate.py
@@ -5,7 +5,17 @@ from model.model import MNISTFC
 from utils.vars import DEVICE, MODEL_PATH
 from utils.utils import set_global_seed
 
-def evaluate_model(model=None, seed=None):
+def load_model():
+    """
+    Loads trained MNISTFC model stored under MODEL_PATH.
+    """
+    print("   Loading trained model...")
+    model = MNISTFC().to(DEVICE)
+    model.load_state_dict(torch.load(MODEL_PATH, map_location=DEVICE, weights_only=True))
+    print("   Done!")
+    return model
+
+def evaluate_model():
     """
     Evaluate the model and return accuracy.
     """
@@ -13,20 +23,20 @@ def evaluate_model(model=None, seed=None):
     set_global_seed()
     # get test data
     test_loader = get_mnist_dataloader(train=False)
-    model = MNISTFC().to(DEVICE)
-    # TODO: ensure that trained model file exists before running evaluate_model
-    model.load_state_dict(torch.load(MODEL_PATH, map_location=DEVICE))
-    model.eval()
 
+    print("\nStarting evaluation...")
+    # load trained model
+    model = load_model()
+    # evaluate
+    model.eval()
     correct = 0
     total = 0
     with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(DEVICE), target.to(DEVICE)
+        for data, label in test_loader:
+            data, label = data.to(DEVICE), label.to(DEVICE)
             outputs = model(data)
             _, predicted = torch.max(outputs.data, 1)
-            total += target.size(0)
-            correct += (predicted == target).sum().item()
+            total += label.size(0)
+            correct += (predicted == label).sum().item()
     accuracy = 100 * correct / total
-
-    return accuracy
+    print(f"Done!\nModel accuracy on the validation set: {accuracy}%")
--- model/train.py
+++ model/train.py
@@ -1,12 +1,14 @@
 import torch
 import torch.optim as optim
 
+import os
+
 from data.data import get_mnist_dataloader
 from model.model import MNISTFC
-from utils.vars import DEVICE, MODEL_PATH
+from utils.vars import DEVICE, MODEL_PATH, EPOCHS, LR
 from utils.utils import set_global_seed
 
-def train_model(epochs=2):
+def train_model(epochs=EPOCHS, lr=LR):
     """
     Train the MNISTFC model and return the trained model.
 
@@ -22,17 +24,40 @@ def train_model(epochs=2):
     # define loss
     criterion = torch.nn.CrossEntropyLoss()
     # define optimizer
-    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    optimizer = optim.SGD(model.parameters(), lr=lr)
 
+    print("\nStarting training...")
+    # training loop
+    correct = 0
+    total = 0
     model.train()
     for epoch in range(epochs):
-        for batch_idx, (data, target) in enumerate(train_loader):
-            data, target = data.to(DEVICE), target.to(DEVICE)
+        running_loss = 0.0  # accumulate loss
+        correct = 0  # count correct predictions
+        total = 0  # count total predictions
+        for batch_idx, (data, label) in enumerate(train_loader):
+            # forward
+            data, label = data.to(DEVICE), label.to(DEVICE)
             optimizer.zero_grad()
             output = model(data)
-            loss = criterion(output, target)
+            # backward
+            loss = criterion(output, label)
             loss.backward()
             optimizer.step()
+            # metric
+            _, predicted = torch.max(output.data, 1)
+            total += label.size(0)
+            correct += (predicted == label).sum().item()
+        # calculate average loss and accuracy for the epoch
+        epoch_loss = running_loss / len(train_loader)
+        accuracy = 100 * correct / total
+        print(f"    Epoch [{epoch+1}/{epochs}], Loss: {epoch_loss:.4f}, Accuracy: {accuracy:.2f}%")
+    accuracy = 100 * correct / total
 
+    # make sure directory where to save the model exists
+    if not os.path.exists(os.path.dirname(MODEL_PATH)):
+        os.makedirs(os.path.dirname(MODEL_PATH))
+    # save trained model
     torch.save(model.state_dict(), MODEL_PATH)
-    return model
+    print(f"Done!\nModel accuracy on the training set after training for {epochs} epochs: {accuracy:.2f}%")
+
