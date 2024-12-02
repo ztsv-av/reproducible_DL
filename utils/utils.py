a51b5737dcce8b728292b72db09a77caa64b9e72
ztsv-av <ztsv.1337@gmail.com>
2024-12-02 17:10:13 +0100

    Trying to fix seed issue

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,22 @@
 import torch
 
+import numpy as np
+import random
+
 from utils.vars import SEED
 
-def set_global_seed() -> None:
+def set_global_seed(seed: int = SEED) -> None:
     """
-    Sets the random seed for PyTorch to ensure reproducibility.
+    Sets the random seed for PyTorch, NumPy, and Python's random module to ensure reproducibility.
+    
+    Parameters:
+        - seed (int): The seed value to set.
     """
-    torch.manual_seed(SEED)
+    # set seed for Python's random module
+    random.seed(seed)
+    # set seed for NumPy
+    np.random.seed(seed)
+    # set seed for PyTorch
+    torch.manual_seed(seed)
+    # ensures PyTorch operations on the CPU are deterministic
+    torch.use_deterministic_algorithms(True)
