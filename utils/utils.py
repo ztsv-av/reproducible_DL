15258376cda9995179b0e4e4f87877c64afd29dd
ztsv-av <ztsv.1337@gmail.com>
2024-12-02 17:22:16 +0100

    remove seed param from set_global_seed

--- utils/utils.py
+++ utils/utils.py
@@ -5,18 +5,15 @@ import random
 
 from utils.vars import SEED
 
-def set_global_seed(seed: int = SEED) -> None:
+def set_global_seed() -> None:
     """
     Sets the random seed for PyTorch, NumPy, and Python's random module to ensure reproducibility.
-    
-    Parameters:
-        - seed (int): The seed value to set.
     """
     # set seed for Python's random module
-    random.seed(seed)
+    random.seed(SEED)
     # set seed for NumPy
-    np.random.seed(seed)
+    np.random.seed(SEED)
     # set seed for PyTorch
-    torch.manual_seed(seed)
+    torch.manual_seed(SEED)
     # ensures PyTorch operations on the CPU are deterministic
     torch.use_deterministic_algorithms(True)
